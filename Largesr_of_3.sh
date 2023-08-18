1d4de1a9cb515321ff890775e703eeffdfbd1a0e
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-08-18 22:19:10 +0530

    Create Largesr_of_3.sh

---
+++ Largesr_of_3.sh
@@ -0,0 +1,18 @@
+//LARGEST OF THREE
+#!bin/bash
+echo "Enter First Element : "
+read a
+echo "Enter Second Element : "
+read b
+echo "Enter Third Element : "
+read c
+max=$a
+if [ $b -gt $max ];
+then
+max=$b
+fi
+if [ $c -gt $max ];
+then
+max=$c
+fi
+echo "$max is the Largest"

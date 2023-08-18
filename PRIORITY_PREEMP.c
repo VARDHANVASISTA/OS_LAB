9f134e30d0f3b94d9746c1f871915a4271db901f
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-08-18 22:35:10 +0530

    Update PRIORITY_PREEMP.c

--- PRIORITY_PREEMP.c
+++ PRIORITY_PREEMP.c
@@ -1,63 +1,65 @@
 //PRIORITY PREEMPTIVE IMPLIMENTATION
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct process
-{
-    int id,at,bt,ct,tat,wt,iscompl,priority;
-}Process;
-void main()
-{
-    int completed=0,curtime=0,minindex,minpriority;
-    printf("Enter the Number of Process :");
-    int n;scanf("%d",&n);
-    Process p[n];
-    printf("Enter The Arrival and Burst Time and Priority\n");
-    for(int i=0;i<n;i++)
-    {
-        p[i].id=i+1;
-        p[i].iscompl=0;
-        printf("P%d----------->",i+1);
-        scanf("%d%d%d",&p[i].at,&p[i].bt,&p[i].priority);
+
+typedef struct process {
+    int Id, AT, BT, CT, TAT, WT, priority; 
+} pro;
+
+pro p[15];
+
+void main() {
+    int n, tempBT[15], total_WT=0, total_TAT=0;
+    float avg_WT=0, avg_TAT=0;
+    printf("\nEnter the number of processes:\n");
+    scanf("%d", &n);
+    printf("\nEnter the arrival time, burst time and priority of the process:\n");
+    printf("AT BT p\n");
+    for(int i=0; i<n; i++) {
+        p[i].Id = (i+1);
+        scanf("%d%d%d", &p[i].AT, &p[i].BT, &p[i].priority);
+        tempBT[i] = p[i].BT;
     }
-    float totaltat=0,totalwt=0;
-    while(completed!=n)
-    {
-        minindex=-1;
-        minpriority=9999;
-        for(int i=0;i<n;i++)
-        {
-            if(p[i].at<=curtime && p[i].iscompl==0)
-            {
-                if(p[i].priority<minpriority || (p[i].priority==minpriority && p[i].at<p[minindex].at))
-                {
-                    minpriority=p[i].priority;
-                    minindex=i;
+
+    printf("\nGantt Chart:\n");
+    int minIndex, minPriority, completed=0, curTime = 0;
+    while(completed != n) {
+        minIndex = -1;
+        minPriority = 9999;
+        for(int i=0; i<n; i++) {
+            if(p[i].AT <= curTime && p[i].BT>0) {
+                if(p[i].priority < minPriority || (p[i].priority == minPriority && p[i].AT < p[minIndex].AT)) {
+                    minPriority = p[i].priority;
+                    minIndex = i;
                 }
             }
         }
-        if(minindex==-1)
-        curtime++;
-        else
-        {
-            curtime+=p[minindex].bt;
-            p[minindex].ct=curtime;
-            p[minindex].tat=p[minindex].ct-p[minindex].at;
-            p[minindex].wt=p[minindex].ct-p[minindex].tat;
-            p[minindex].iscompl=1;
-            totaltat+=p[minindex].tat;
-            totalwt+=p[minindex].wt;
-            completed++;
-            printf("|P%d (%d) %d",p[minindex].id,p[minindex].bt,p[minindex].ct);
 
+        curTime++;
+
+        if (minIndex != -1)
+        {
+            p[minIndex].BT--;
+            printf("| P%d(1) %d", p[minIndex].Id, curTime);
+            if (p[minIndex].BT == 0)
+            {
+                p[minIndex].CT = curTime;
+                p[minIndex].TAT = p[minIndex].CT - p[minIndex].AT;
+                p[minIndex].WT = p[minIndex].TAT - tempBT[minIndex];
+                total_TAT += p[minIndex].TAT;
+                total_WT += p[minIndex].WT;
+                completed++;
+            }
         }
     }
     printf("|\n");
-    float avg_TAT = (float)totaltat/n;
-    float avg_WT = (float)totalwt/n;
-    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\tPriority\n");
+    avg_TAT = (float)total_TAT/n;
+    avg_WT = (float)total_WT/n;
+
+    //Printing the table of processes with details
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\tP\n");
     for(int i=0; i<n; i++) {
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].id, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt,p[i].priority);
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].Id, p[i].AT, tempBT[i], p[i].CT, p[i].TAT, p[i].WT, p[i].priority);
     }
     printf("\nAverage TAT = %.2f\nAverage WT = %.2f\n", avg_TAT, avg_WT);
-
 }

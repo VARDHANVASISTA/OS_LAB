88b5b0ffd50d5344064256578935b5082a34d1cf
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-08-18 22:24:48 +0530

    Create prime.sh

---
+++ prime.sh
@@ -0,0 +1,18 @@
+//PRIME NUMBERS IN RANGE
+#!bin/bash
+echo "Enter The Number : "
+read n
+temp=$n
+rev=0
+while(($n>0))
+do
+r=$((n%10))
+rev=$((rev*10+r))
+n=$((n/10))
+done
+if [ $temp -eq  $rev ];
+then
+echo "Palindrome"
+else
+echo "Not Palindrome"
+fi
